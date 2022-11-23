29dff9c24ca27a62832c4f13cb47dc31f2086dd0
PedroSanz93 <118895052+PedroSanz93@users.noreply.github.com>
2022-11-23 20:57:56 +0100

    Add files via upload

---
+++ Ejercicios_Bucles/Ej03FUNCION.java
@@ -0,0 +1,22 @@
+
+package Ejercicios_Bucles;
+
+
+public class Ej03FUNCION {
+
+    public static void multiplosde5DOWHILE()
+    {
+        int i=0;
+        do
+        {
+            i=i+5;
+            System.out.println(i);
+        } while (i<100);
+    }
+
+    public static void main(String[] args) {
+        
+        multiplosde5DOWHILE();
+    }
+    
+}
