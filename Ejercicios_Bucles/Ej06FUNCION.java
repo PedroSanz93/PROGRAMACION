6cc450a7b23ff3dcf3ec27a94934e0aa4944a9cf
PedroSanz93 <118895052+PedroSanz93@users.noreply.github.com>
2022-11-23 21:06:22 +0100

    Add files via upload

---
+++ Ejercicios_Bucles/Ej06FUNCION.java
@@ -0,0 +1,19 @@
+
+package Ejercicios_Bucles;
+
+public class Ej06FUNCION {
+
+    public static void cuenta320DOWHILE()
+    {
+        int i=320;
+        do
+        {
+            i=i-20;
+            System.out.println(i);
+        } while (i>160);
+    }
+    
+    public static void main(String[] args) {
+            cuenta320DOWHILE();
+    }
+}
