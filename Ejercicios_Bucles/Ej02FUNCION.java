1f57ee3371c825c7c4635a617f6fb35170b9c9cd
PedroSanz93 <118895052+PedroSanz93@users.noreply.github.com>
2022-11-23 20:54:53 +0100

    Add files via upload

---
+++ Ejercicios_Bucles/Ej02FUNCION.java
@@ -0,0 +1,21 @@
+
+package Ejercicios_Bucles;
+
+
+public class Ej02FUNCION {
+
+    public static void multiplo5WHILE()
+    {
+        int i=0;
+        System.out.println("multiplos de 5");
+        while (i<100){
+            i=i+5;
+            System.out.println(i);
+            
+        }
+    }
+
+    public static void main(String[] args) {
+            multiplo5WHILE();
+    }
+}
