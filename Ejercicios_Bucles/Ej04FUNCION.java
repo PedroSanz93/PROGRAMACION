58704dda0db589f36cc361d9952f6f5bf88c6b27
PedroSanz93 <118895052+PedroSanz93@users.noreply.github.com>
2022-11-23 21:03:47 +0100

    Add files via upload

---
+++ Ejercicios_Bucles/Ej04FUNCION.java
@@ -0,0 +1,18 @@
+
+package Ejercicios_Bucles;
+
+
+public class Ej04FUNCION {
+
+    public static void cuenta320FOR()
+    {
+        for(int i=320;i>=160;i=i-20){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        
+            cuenta320FOR();
+    }
+}
---
+++ Ejercicios_Bucles/Ej05FUNCION.java
@@ -0,0 +1,20 @@
+
+package Ejercicios_Bucles;
+
+
+public class Ej05FUNCION {
+
+    public static void cuenta320WHILE()
+    {
+        int i=320;
+        while (i>160){
+            i=i-20;
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+            cuenta320WHILE();
+    }
+    
+}
