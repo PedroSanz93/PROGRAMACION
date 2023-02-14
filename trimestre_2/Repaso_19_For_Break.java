7e8d5ee6fde106029fbbb27749b8477aa66f1550
PedroSanz93 <118895052+PedroSanz93@users.noreply.github.com>
2023-02-14 18:40:51 +0100

    Add files via upload

---
+++ trimestre_2/Repaso_19_For_Break.java
@@ -0,0 +1,13 @@
+
+package trimestre_2;
+
+public class Repaso_19_For_Break {
+
+    public static void main(String[] args) {
+        System.out.println("Inicio: ");
+        for (int i=1;i<=10;i++){
+            if(i==5) break;
+            System.out.println(i);
+        }
+    } 
+}
