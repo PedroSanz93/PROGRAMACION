2d0491f2be2b69d00003736c0edda660970c77fd
PedroSanz93 <118895052+PedroSanz93@users.noreply.github.com>
2023-04-29 15:42:17 +0200

    Update Fabricante.java

--- Practica_09_Clases/Fabricante.java
+++ Practica_09_Clases/Fabricante.java
@@ -1,21 +1,18 @@
-
 package Practica_09_Clases;
 
 //SUBLASE FABRICANTE
-
 class Fabricante extends TiendaRopa{
-    
+    // atributos propios de Fabricante
     public int CODmaterial;
     public int fechaFab;
     // constructor
     public Fabricante(int IDTienda, double precio, int CODmaterial) {
-        // con super llamo a los atributos de la superclase
+        // con super() llamo a los atributos de la superclase
         super(IDTienda, precio);
-        // y estos son los atributos propios de la subclase fabricante
         this.CODmaterial = CODmaterial;
         this.fechaFab = fechaFab;
     }
-    // Getters y Setters para obtener la informacion del fabricante
+    // Metodos Getters y Setters de Fabricante
     public void setCODmaterial(int marca){
         this.CODmaterial = CODmaterial;
     }
